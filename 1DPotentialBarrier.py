b0c46c4597acd6c23a8d5c23d42b03bef0e48800
Amy Shaddick <66920332+spindash04@users.noreply.github.com>
2025-11-14 15:00:54 +0000

    Update 1DPotentialBarrier.py

--- 1DPotentialBarrier.py
+++ 1DPotentialBarrier.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 28 00:45:26 2024
-
-@author: 08sha
-"""
-
-## program uses units such that hbar = m = 1
 
 #%% solving time dependent schrodinger equation 
 
@@ -235,4 +228,5 @@ for i in range(len(timestamps)):
 plt.xlabel('Position [a.u.]')
 plt.ylabel('Probability')
 plt.legend(('Scaled potential barrier', 't= '+ str(int(timestamps[0]/100)), 't= '+ str(int(timestamps[1]/100)), 't= '+ str(int(timestamps[2]/100))), loc='upper right')
+
 plt.title('Probability densities at a range of times')
